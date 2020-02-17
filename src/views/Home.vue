ce3b1d9e7a50ca631fbc4f138e278cb6c437110f
Gregory Anne <ganne@effisoft-group.com>
2020-02-17 11:50:21 +0100

    repo autonome

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Greg's TS-Vue.js/Nest.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Calculator from '@/components/Calculator.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Calculator
+  }
+}
+</script>
